eaeb9357c143f9a942c1eaa166985ca5f0ce5c93
HunterBjj <a.urmanow@mail.ru>
2026-07-15 15:02:43 +0700

    Create test_assignment.sql
    Add function template and schema.

---
+++ test_assignment.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS dbo;
+
+CREATE TABLE IF NOT EXISTS dbo.fp_ayment_split(
+);
+
+CREATE TABLE IF NOT EXISTS dbo.fd_payments (
+);
+
+CREATE TABLE IF NOT EXISTS dbo.fd_bills (
+);
+
