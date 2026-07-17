4a3efc82ae3229dc0cdfbdf136851aa69f745015
HunterBjj <a.urmanow@mail.ru>
2026-07-17 10:34:51 +0700

    Update test_assignment.sql
    Add check amount.

--- test_assignment.sql
+++ test_assignment.sql
@@ -7,19 +7,19 @@
 CREATE SCHEMA IF NOT EXISTS dbo;
 
 CREATE TABLE IF NOT EXISTS dbo.fd_payment_details(
-  id_fd_payment_details SERIAL PRIMARY KEY,
+  id_fd_payment_details  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,,
 );
 
 CREATE TABLE IF NOT EXISTS dbo.fd_payments (
-  id_fd_payments SERIAL PRIMARY KEY,
-  c_number,
+  id_fd_payments INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  c_number VARCHAR(50),
   f_subscr INT NOT NULL,
   d_date DATE NOT NULL,
   n_amount NUMERIC(15,2)
 );
 
 CREATE TABLE IF NOT EXISTS dbo.fd_bills (
-  id_fd_bills SERIAL PRIMARY KEY,
+  id_fd_bills  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,,
 );
 
 CREATE OR REPLACE FUNCTION dbo.ui_fp_payment_split(
@@ -28,22 +28,50 @@ CREATE OR REPLACE FUNCTION dbo.ui_fp_payment_split(
 ) 
 RETURNS VOID AS $$
 DECLARE
-
+  _p_subscr INT;
+  _p_amount NUMERIC(15,2);
+  _r RECORD;
+  _pay_part NUMERIC(15,2);
+  _month_total_rest NUMERIC(15,2);
+  _month_total_pay NUMERIC(15,2);
 BEGIN
-  PERFORM 1 
-  FROM dbo.fd_payments 
-  WHERE link = _link 
-  FOR UPDATE;
-  
-  IF EXISTS(SELECT 1 FROM dbo.fb_payement_details WHERE f_payment = p_payment_id)
-    DELETE FROM dbo.fd_payment_details WHERE f_payment = p_payment_id;
-  END IF;
+  BEGIN
+    PERFORM 1 
+    FROM dbo.fd_payments 
+    WHERE link = _link 
+    FOR UPDATE;
+    
+    IF EXISTS(SELECT 1 FROM dbo.fb_payement_details WHERE f_payment = p_payment_id)
+      UPDATE dbo.fd_bills b
+      SET n_rest = b.n_rest + pd.n_amount
+      FROM dbo.fd_payment_details pd
+      WHERE pd.f_bill = b.link AND pd.f_payment = p_payment_id
+      FOR UPDATE OF b; 
+      
+      DELETE FROM dbo.fd_payment_details WHERE f_payment = p_payment_id;
+    END IF;
 
-  IF p_split_type = 0 THEN
-  END IF;
-  ELSIF p_split_type = 1 THEN
-  END IF;
+    SELECT f_subscr, n_amount
+    INTO _p_subscr, _p_amount
+    FROM dbo.fd_payments
+    WHERE id_fd_payments = p_payments_id
+    FOR UPDATE;
 
+    IF _p_amount <= 0 THEN
+      RAISE EXCEPTION 'Платеж % не должен быть меньши или равен нулю', p_payments_id;
+    END IF;
+    
+    IF p_split_type = 0 THEN
+      FOR _r IN (
+         SELECT id_fb_bills, n_rest
+      ) LOOP
+          
+    END IF;
+
+    ELSIF p_split_type = 1 THEN
+    END IF;
+
+  END;
 EXCEPTION 
     WHEN OTHERS THEN
 
